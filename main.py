a2f9993a575bd9ea008196686ef95a01307b5bf1
saamuelkp <kpetemeys@gmail.com>
2026-03-17 19:07:49 -0400

    Ajout CORS pour connexion frontend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 from database import db, User, Transaction, Budget
 from auth import auth_bp
 from transactions import transactions_bp
@@ -12,6 +13,7 @@ app.config['JWT_SECRET_KEY'] = 'budgetwise-jwt-secret'
 
 db.init_app(app)
 jwt = JWTManager(app)
+CORS(app)
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(transactions_bp)
