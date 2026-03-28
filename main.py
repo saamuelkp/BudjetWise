d564327b90d3d79ab2fad9a53991728da6f248bc
slimb-hash <TON_EMAIL_GITHUB>
2026-03-27 23:07:22 -0400

    fix CORS

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_jwt_extended import JWTManager
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=False)
+from database import db, User, Transaction, Budget
+from auth import auth_bp
+from transactions import transactions_bp
+from analytics import analytics_bp
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///budgetwise.db'
+app.config['SECRET_KEY'] = 'budgetwise-secret-key'
+app.config['JWT_SECRET_KEY'] = 'budgetwise-jwt-secret'
+
+db.init_app(app)
+jwt = JWTManager(app)
+CORS(app, origins="*", supports_credentials=True)
+
+app.register_blueprint(auth_bp)
+app.register_blueprint(transactions_bp)
+app.register_blueprint(analytics_bp)
+
+@app.route('/')
+def home():
+    return {'message': 'BudgetWise API v2 fonctionne !'}
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug=True)
