fc2653a509dad4834c61a9f6e6e106a62878cbcc
saamuelkp <kpetemeys@gmail.com>
2026-03-28 00:33:05 -0400

    Reset DB pour nouveaux champs

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def home():
     return {'message': 'BudgetWise API v2 fonctionne !'}
 
 with app.app_context():
+    db.drop_all()
     db.create_all()
-
 if __name__ == '__main__':
     app.run(debug=True)
